63c6a031a456a4c8a61df07f7ddca392730f6f7c
CodeNinjasKW <codeninjacloud@gmail.com>
2022-02-26 13:17:42 -0500

    Working on MeanyBird

---
+++ MeanyBirdEverett/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    //game manager object
+    [Header("Game Controller Object for controlling the game")]
+    public GameController gameController;
+    [Header("Default Velocity")]
+    public float velocity = 5;
+    //physics for the bird
+    private Rigidbody2D rb;
+    //height of the bird object on the y axis
+    private float objectHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //game controller component
+        gameController = GetComponent<GameController>();
+        //speed for the game is at a playing state
+        Time.timeScale = 1;
+        rb = GetComponent<Rigidbody2D>();
+        //Object height equals the size of the height of the sprite
+        objectHeight =
+            transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the left mouse button is clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            //the bird will float up on the y axis and float back down on the y axis
+            rb.velocity = Vector2.up * velocity;
+        }
+
+    }
+}
---
+++ MeanyBirdEverett/Assets/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //Object we will attach to script for spawning object
+    [Header("Spikes Object for controlling the game")]
+    public GameObject spikes;
+    //Height position of the spikes
+    [Header("Default Height")]
+    public float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start function repeating every 4 seconds
+        InvokeRepeating("InstantiateObjects", 1f, 4f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(5, Random.Range(-height, height), 0);
+
+    }
+
+    //instantiateObjects function
+    void InstantiateObjects()
+    {
+        //Spawn object by position and rotation
+        Instantiate(spikes, transform.position, transform.rotation);
+    }
+
+}
